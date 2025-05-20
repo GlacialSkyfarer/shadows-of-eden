a1f05121e21ce4719a1509ec08fedc2e3d096779
GlacialSkyfarer <curseflame1122@gmail.com>
2025-05-19 23:22:36 -0600

    Added a secondary reticle always in the center of the screen -Raine

--- Entities/Behaviours/PlayerCrosshair.cs
+++ Entities/Behaviours/PlayerCrosshair.cs
@@ -26,13 +26,11 @@ public partial class PlayerCrosshair : Behaviour
     {
 
         raycast.TargetPosition = raycast.ToLocal(aimPoint.Target);
-        raycast.Enabled = true;
         raycast.ForceRaycastUpdate();
-        raycast.Enabled = false;
 
-        if (raycast.IsColliding()) { crosshair.GlobalPosition = crosshair.GlobalPosition.Lerp(raycast.GetCollisionPoint(), (float)delta * 20f); }
+        if (raycast.IsColliding()) { crosshair.GlobalPosition = crosshair.GlobalPosition.Lerp(raycast.GetCollisionPoint(), (float)delta * 30f); }
         else
-        { crosshair.GlobalPosition = crosshair.GlobalPosition.Lerp(raycast.ToGlobal(raycast.TargetPosition), (float)delta * 20f); }
+        { crosshair.GlobalPosition = crosshair.GlobalPosition.Lerp(raycast.ToGlobal(raycast.TargetPosition), (float)delta * 30f); }
 
     }
 
